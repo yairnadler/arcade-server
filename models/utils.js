211025027852d2ff6e59cf3471edc0faf9e32c03
Yair Nadler <yair.nadler7@gmail.com>
2024-09-26 10:39:56 +0300

    same ol same

--- models/utils.js
+++ models/utils.js
@@ -1,7 +1,6 @@
 import { ArcadeGameModel } from "./ArcadeGames.js";
 import mongoose from "mongoose";
 
-
 export default function connect(mongoPassword){
   mongoose.connect(
     `mongodb+srv://yairnadler7:${mongoPassword}@cluster0.s7myk.mongodb.net/arcade-app`
@@ -10,5 +9,6 @@ export default function connect(mongoPassword){
 
 export async function fetchGamesFromDB() {
   const games = await ArcadeGameModel.find().select("_id title image link"); 
+
   return games;
 }
